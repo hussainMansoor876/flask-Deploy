ff88b62a4bccc29d66d8b5430ed8c4c3c28ebf3b
Mansoor <hussainmansoor876@gmail.com>
2019-12-05 19:24:18 +0500

    Finalized

--- app.py
+++ app.py
@@ -1,26 +1,41 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import pandas as pd
-import random
 
-data = pd.read_excel('data.xlsx', usecols='B,C,D,E,F,G') # Read data from excel
-dict1 = {}
 
 app = Flask(__name__)
 
 
-@app.route('/') #Return the form
+@app.route('/')
 def index():
-    for i in data:
-        ind = random.randint(0, int(len(data[i].dropna())-1)) #generate the random number between 0 and len of column
-        dict1[i] = data[i][ind] #assign the key and value base on random value
     return render_template('index.html')
 
 
 @app.route('/result', methods=["POST"])
 def result():
-    req = dict(request.form)
-    return render_template('result.html', req=req, dict1=dict1, keys=data.columns)
+    file = request.files['file']
+    try:
+        data = pd.read_csv(file, header=None)
+        totalTime = ((data[0][0] * 10) + data[1][0]) + ((len(data[0][data[0] == 0]) - 1)
+                                                        * 1024 * 10) + ((data[0].iloc[-1] * 10) + data[1].iloc[-1])
+        removeDup = data[1].unique()
+        legend = 'Result'
+        data1 = data.groupby([0, 1]).sum()
+        x = data1.reset_index()
+        labels = ["January", "February", "March",
+                "April", "May", "June", "July", "August"]
+        values = [10, 9, 8, 7, 6, 4, 7, 8]
+        return render_template('result.html', totalTime=totalTime, removeDup=removeDup,  values=x[5], labels=x[1], legend=legend)
+    except:
+        data = pd.read_excel(file, header=None)
+        totalTime = ((data[0][0] * 10) + data[1][0]) + ( (len(data[0][data[0] == 0]) - 1) * 1024 * 10) + ((data[0].iloc[-1] * 10) + data[1].iloc[-1])
+        removeDup = data[1].unique()
+        legend = 'Result'
+        data1 = data.groupby([0, 1]).sum()
+        x = data1.reset_index()
+        labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
+        values = [10, 9, 8, 7, 6, 4, 7, 8]
+        return render_template('result.html', totalTime=totalTime, removeDup=removeDup,  values=x[5], labels=x[1], legend=legend)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
